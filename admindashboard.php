fbc738b2ba16d37e7c6972a56fe009ca9ae899e2
Prince Chukwudire <chimeremezechukwudire@gmail.com>
2020-04-10 00:06:37 +0100

    admin dashboard added

---
+++ admindashboard.php
@@ -0,0 +1,12 @@
+<?php include_once('lib/header.php'); 
+
+if(!isset($_SESSION['loggedIn'])){
+    // redirect to dashboard
+    header("Location: login.php");
+}
+?>
+<h3> Admin Dashboard</h3>
+
+Welcome, <?php echo $_SESSION['fullname'] ?>, You are logged in as (<?php echo $_SESSION['role'] ?>), and your ID is <?php echo $_SESSION['loggedIn'] ?>.
+<?php echo $_SESSION['loggedin_at'] ?>
+<?php include_once('lib/footer.php'); ?>
