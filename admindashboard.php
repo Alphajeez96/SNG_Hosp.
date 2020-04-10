1827916f06973badb89230bfd0f9ff9f1f87c5f0
Prince Chukwudire <chimeremezechukwudire@gmail.com>
2020-04-10 20:11:38 +0100

    db modified

--- admindashboard.php
+++ admindashboard.php
@@ -148,14 +148,14 @@ if(!isset($_SESSION['loggedIn'])){
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Admin  Dashboard</h1>
-            <a href="register.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-user fa-sm text-white-50"></i> Add User</a>
+            <!-- <a href="register.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-user fa-sm text-white-50"></i> Add User</a> -->
           </div>
 
           <!-- Content Row -->
           <div class="row">
 
             <!-- Earnings (Monthly) Card Example -->
-            <!-- <div class="col-xl-4 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -169,7 +169,7 @@ if(!isset($_SESSION['loggedIn'])){
                   </div>
                 </div>
               </div>
-            </div> -->
+            </div>
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-6 col-md-6 mb-4">
