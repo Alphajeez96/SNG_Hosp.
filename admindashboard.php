c345c738d7ebfb5adc89241362c2a198edd3b7de
Prince Chukwudire <chimeremezechukwudire@gmail.com>
2020-04-23 17:08:06 +0100

    admin dashboard refactored

--- admindashboard.php
+++ admindashboard.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION['loggedIn'])){
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="dashboard.php">
+        <a class="nav-link" href="admindashboard.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -40,15 +40,15 @@ if(!isset($_SESSION['loggedIn'])){
 
       <li class="nav-item">
         <a class="nav-link" href="allstaff.php">
-        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+        <i class="fas fa-user-md fa-sm fa-fw mr-2 text-gray-400"></i>
           <span>Staff</span></a>
       </li>
       <hr class="sidebar-divider">
 
       <li class="nav-item">
         <a class="nav-link" href="allpatients.php">
-        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-          <span>Patient</span></a>
+        <i class="fas fa-user-friends fa-sm fa-fw mr-2 text-gray-400"></i>
+          <span>Patients</span></a>
       </li>
       <hr class="sidebar-divider">
 
@@ -215,30 +215,7 @@ if(!isset($_SESSION['loggedIn'])){
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-6 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Appointments</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0%</div>
-                        </div>
-                        <div class="col">
-                          <div class="progress progress-sm mr-2">
-                            <div class="progress-bar bg-info" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
            
           </div>
          
--- allstaff.php
+++ allstaff.php
@@ -1,128 +1,129 @@
 <?php include_once('lib/header.php'); ?>
 
 <body id="page-top">
-<!-- Page Wrapper -->
-<div id="wrapper">
 
-  <!-- Sidebar -->
-  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="mtdashboard.php">
-      <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
-      </div>
-      <div class="sidebar-brand-text mx-1">logged in as (<?php echo $_SESSION['role'] ?>)</div>
-    </a>
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admindashboard.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-1">logged in as (<?php echo $_SESSION['role'] ?>)</div>
+      </a>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-      <a class="nav-link" href="mtdashboard.php">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
-    </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="admindashboard.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
 
-    <li class="nav-item">
-      <a class="nav-link" href="radappointments.php">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Staff</span></a>
-    </li>
-    <hr class="sidebar-divider">
-    <!-- Heading -->
+      <!-- Divider -->
+      <hr class="sidebar-divider">
 
-    <li class="nav-item">
-      <a class="nav-link" href="reset.php">
-      <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-        <span>Reset Password</span></a>
-    </li>
+      <li class="nav-item">
+        <a class="nav-link" href="allstaff.php">
+        <i class="fas fa-user-md fa-sm fa-fw mr-2 text-gray-400"></i>
+          <span>Staff</span></a>
+      </li>
+      <hr class="sidebar-divider">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-          <li class="nav-item">
-      <a class="nav-link" href="logout.php">
-      <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-        <span>Logout</span></a>
-    </li>
+      <li class="nav-item">
+        <a class="nav-link" href="allpatients.php">
+        <i class="fas fa-user-friends fa-sm fa-fw mr-2 text-gray-400"></i>
+          <span>Patients</span></a>
+      </li>
+      <hr class="sidebar-divider">
 
-  </ul>
-  <!-- End of Sidebar -->
+      <li class="nav-item">
+        <a class="nav-link" href="reset.php">
+        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+          <span>Reset Password</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <!-- Heading -->
+            <li class="nav-item">
+        <a class="nav-link" href="logout.php">
+        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+          <span>Logout</span></a>
+      </li>
 
-  <!-- Content Wrapper -->
-  <div id="content-wrapper" class="d-flex flex-column">
+    </ul>
+    <!-- End of Sidebar -->
 
-    <!-- Main Content -->
-    <div id="content">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Topbar -->
-      <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+      <!-- Main Content -->
+      <div id="content">
 
-        <!-- Sidebar Toggle (Topbar) -->
-        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-          <i class="fa fa-bars"></i>
-        </button>
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-        <!-- Topbar Search --> 
-      
-      
-       
-       
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
 
-        <!-- Topbar Navbar -->
-        <ul class="navbar-nav ml-auto">
+          <!-- Topbar Search -->
+        
 
-          <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-          <li class="nav-item dropdown no-arrow d-sm-none">
-            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-search fa-fw"></i>
-            </a>
-            <!-- Dropdown - Messages -->
-            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-              <form class="form-inline mr-auto w-100 navbar-search">
-                <div class="input-group">
-                  <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                  <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                      <i class="fas fa-search fa-sm"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </li>
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
 
-          <!-- Nav Item - Alerts -->
-          <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-bell fa-fw"></i>
-              <!-- Counter - Alerts -->
-              <span class="badge badge-danger badge-counter">1</span>
-            </a>
-            <!-- Dropdown - Alerts -->
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-              <h6 class="dropdown-header">
-                Alerts Center
-              </h6>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="mr-3">
-                  <div class="icon-circle bg-primary">
-                    <i class="fas fa-file-alt text-white"></i>
-                  </div>
-                </div>
-                <div>
-                  <div class="small text-gray-500"><?php echo $_SESSION['register_at'] ?> </div>
-                  <span class="font-weight-bold">Welcome to SNG Hospital, Thanks For Creating an Account</span>
-                </div>
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
               </a>
-              
-              <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-            </div>
-          </li>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+
+            <!-- Nav Item - Alerts -->
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-bell fa-fw"></i>
+                <!-- Counter - Alerts -->
+                <span class="badge badge-danger badge-counter"></span>
+              </a>
+              <!-- Dropdown - Alerts -->
+              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                <h6 class="dropdown-header">
+                  Alerts Center
+                </h6>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                  <div class="mr-3">
+                    <div class="icon-circle bg-primary">
+                      <!-- <i class="fas fa-file-alt text-white"></i> -->
+                    </div>
+                  </div>
+                  <div>
+                   
+                </a>
+                
+                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+              </div>
+            </li>
 
           
           <!-- Nav Item - User Information -->
@@ -175,7 +176,7 @@
                       <th>Designation</th>
                       <th>Department</th>
                       <th>Date Joined</th>
-                      <th>Last Login</th>
+                     
                     </tr>
                   </thead>
 
