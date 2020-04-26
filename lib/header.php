34e852d30e764a3bb384caab237357ca2fb7e41c
Prince Chukwudire <48554942+Alphajeez96@users.noreply.github.com>
2020-04-26 12:02:45 +0100

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to SNG : Hospital for the ignorant</title>
+    <title>Welcome to Alpha-Clinic : Hospital for the ignorant</title>
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
@@ -27,7 +27,7 @@
             <?php if(!isset($_SESSION['loggedIn'])){ ?>
                 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
                 
-                <h5 class="my-0 mr-md-auto font-weight-normal"> <a href="index.php">StartNG Hospital</a></h5>
+                <h5 class="my-0 mr-md-auto font-weight-normal"> <a href="index.php">Alpha-Clinic</a></h5>
                 
         <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-dark" href="index.php">Home</a>
